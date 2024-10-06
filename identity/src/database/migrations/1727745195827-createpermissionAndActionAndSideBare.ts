364457c9adf0bddfd56065f08384b7791f3637a5
MohammedGhanem1997 <102333174+MohammedGhanem1997@users.noreply.github.com>
2024-10-06 08:22:55 -0700

    Update 1727745195827-createpermissionAndActionAndSideBare.ts

--- identity/src/database/migrations/1727745195827-createpermissionAndActionAndSideBare.ts
+++ identity/src/database/migrations/1727745195827-createpermissionAndActionAndSideBare.ts
@@ -1,14 +1,14 @@
-import { getRepository, MigrationInterface, QueryRunner } from "typeorm";
-import { actionSeed } from "../seeds/action.seed";
-export class CreatepermissionAndActionAndSideBare1727745195827 implements MigrationInterface {
+// import { getRepository, MigrationInterface, QueryRunner } from "typeorm";
+// import { actionSeed } from "../seeds/action.seed";
+// export class CreatepermissionAndActionAndSideBare1727745195827 implements MigrationInterface {
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
+//     public async up(queryRunner: QueryRunner): Promise<void> {
         
-        const actions: any = actionSeed;
-        await queryRunner.manager.save('action', actions);
-    }
+//         const actions: any = actionSeed;
+//         await queryRunner.manager.save('action', actions);
+//     }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
+//     public async down(queryRunner: QueryRunner): Promise<void> {
+//     }
 
-}
+// }
