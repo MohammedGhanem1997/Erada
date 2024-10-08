b494d1e3d99bf9ba7b3f59ac6e89f2238bf30461
MohammedGhanem1997 <102333174+MohammedGhanem1997@users.noreply.github.com>
2024-10-08 16:51:10 +0300

    Update entities.ts

--- vas/src/types/entities.ts
+++ vas/src/types/entities.ts
@@ -31,14 +31,22 @@ export interface IBranch extends IBaseWithMeta{
   staffs?:any
 }
 
-export interface IEmployee extends IBaseWithId{
 
-  branch: string;
-  staff?:any
-  staffId:string
+export interface IInsuranceCompany extends IBaseWithMeta {
+  name: string;
+  bankName: string;
+  accountNumber: string;
+  eradaAccountNumber: string;
+  status: boolean;
+  policiesCount: number;
 }
 
-
-
+export interface IInsurancePolicy extends IBaseWithMeta {
+  insuranceCompanyId: string;
+  name: string;
+  amount: number;
+  startDate: Date;
+  endDate: Date;
+}
 
 
